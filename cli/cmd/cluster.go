a117d905fcd7377c5c7bfea1b78a6cf00a2debe9
tkepe <tiago.kepe@enterprisedb.com>
2026-07-27 19:26:20 +0000

    WIP: gplog.Error()

--- cli/cmd/cluster.go
+++ cli/cmd/cluster.go
@@ -143,7 +143,7 @@ func doSetup() (*ClusterData, error) {
 	}
 	segConfigs, err := cluster.GetSegmentConfiguration(connection, true)
 	if err != nil {
-		gplog.Error("ERROR: Could not retrieve segment information from GPDB.\n%s\n" + err.Error())
+		gplog.Error("ERROR: Could not retrieve segment information from GPDB.\n%s\n", err.Error())
 		return nil, err
 	}
 	clusterData := &ClusterData{Cluster: cluster.NewCluster(segConfigs), connection: connection}
